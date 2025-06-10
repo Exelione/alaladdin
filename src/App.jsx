2599cc02081bbdf2ae4869fd7b1e94c0cd6e0c84
Exelione <exelione@gmail.com>
2025-06-10 23:00:04 +0300

    feat/1:ContainerLogoLinkStyles

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "./components/Sidebar";
 
+
 library.add(fas);
 
 export default class App extends React.Component{
   render () {
       return (
-          <Sidebar color='light' />
+          <Sidebar color='dark' />
       )
   }
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import classnames from 'classnames';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../assets/logo.png';
 import PropTypes from 'prop-types';
@@ -17,60 +17,172 @@ const bottomRoutes = [
     { title: 'Settings', icon: 'sliders', path: '/settings' },
     { title: 'Support', icon: 'phone-volume', path: '/support' },
 ];
+const SidebarContainer = styled.div`
+    background-color: ${({ theme }) =>
+        theme === 'dark'
+            ? `var(--color-sidebar-background-dark-default)`
+            : `var(--color-sidebar-background-light-default)`
+    };
+    color: ${({ theme }) =>
+        theme === 'dark'
+            ? `var(--color-text-dark-default)`
+            : `var(--color-text-light-default)`
+    };
+    width: ${({ open }) => open ? '250px' : '50px'};
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: ${({ open }) => open ? 'flex-start' : 'center'};
+    border-radius: 16px;
+   
+    border: ${({ theme }) =>
+        theme === 'dark'
+            ? '2px solid transparent'
+            : '2px solid transparent'
+    };
+    box-shadow: inset 0 0 0 2px
+     ${({ theme }) =>
+        theme === 'dark'
+            ? `var(--color-button-background-dark-active)`
+            : `var(--color-button-background-light-active)`};
+
+    transition: all 0.3s ease-in-out;
+`;
+
+
+
+const LogoContainer = styled.div`
+    position: relative;
+    overflow: ${({ open }) => (open ? 'hidden' : 'visible')};
+    width: 88%;
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    justify-content: ${({ open }) => open ? 'flex-start' : 'center'};
+    img {
+    width: 25px;
+    height: 25px;
+    };
+
+    span {
+    font-size: 1.2rem;
+    font-weight: bold;
+    display: ${({ open }) => open ? 'block' : 'none'
+    };
+    color: ${({ theme }) =>
+        theme === 'dark'
+            ? `var(--color-text-logo-dark-default)`
+            : `var(--color-text-logo-light-default)`
+    };
+    margin-left: 0.2rem;
+    };
+    z-index: 12;
+    button {
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
+    border-radius: 50%;
+    border: none;
+    background-color: ${({ theme, open }) =>
+        (open && theme === 'dark') ? 'var(--color-button-background-dark-active)' :
+            (open && theme === 'light') ? 'var(--color-button-background-light-active)' :
+                (!open && theme === 'dark') ? 'var(--color-button-background-dark-default)' :
+                    (!open && theme === 'light') && 'var(--color-button-background-light-default)'
+    };
+    
+    right: ${({ open }) => open ? '-0.6rem' : '-2.5rem'};
+    color: ${({ theme }) =>
+        theme === 'dark'
+            ? 'var(--color-text-dark-default)'
+            : 'var(--color-text-light-default)'
+    };
+  
+};
+`
+const LinksContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: ${({ open }) => open ? 'flex-start' : 'center'};
+    width: auto;
+    margin: 2rem 0;
+    padding: 1rem;
+    gap: 1.5rem;
+   
+    span {
+    font-size: 1rem;
+    margin-left: 1rem;
+    display: ${({ open }) => open ? 'block' : 'none'};
+    }
+`
+const LinkElement = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== 'active',
+})`
+    background-color: ${({ active }) => active ? 'green' : 'transparent'};
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+`
+
 
 const Sidebar = (props) => {
-    const { color } = props;
-    const [isOpened, setIsOpened] = useState(false);
-    const containerClassnames = classnames('sidebar', { opened: isOpened });
+
+    const [opened, setOpened] = useState(false);
+    const [active, setActive] = useState('/sales');
 
     const goToRoute = (path) => {
         console.log(`going to "${path}"`);
     };
 
     const toggleSidebar = () => {
-        setIsOpened(v => !v);
+        setOpened(v => !v);
     };
 
     return (
-        <div className={ containerClassnames }>
-            <div>
-                <img src={ logo } alt="TensorFlow logo"/>
+        <SidebarContainer open={opened} theme={props.color}>
+
+            <LogoContainer open={opened} theme={props.color}>
+                <img src={logo} alt="TensorFlow logo" />
                 <span>TensorFlow</span>
-                <div onClick={ toggleSidebar }>
-                    <FontAwesomeIcon icon={ isOpened ? 'angle-left' : 'angle-right' }/>
-                </div>
-            </div>
-            <div>
+                <button onClick={toggleSidebar}>
+                    <FontAwesomeIcon icon={opened ? 'angle-left' : 'angle-right'} />
+                </button>
+            </LogoContainer>
+            <LinksContainer theme={props.color} open={opened}>
                 {
                     routes.map(route => (
-                        <div
-                            key={ route.title }
+                        <LinkElement
+                            key={route.title}
                             onClick={() => {
                                 goToRoute(route.path);
+                                setActive(route.path);
                             }}
+                            active={active === route.path}
                         >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
-                        </div>
+                            <FontAwesomeIcon icon={route.icon} />
+                            <span>{route.title}</span>
+                        </LinkElement>
                     ))
                 }
-            </div>
+            </LinksContainer>
             <div>
                 {
                     bottomRoutes.map(route => (
                         <div
-                            key={ route.title }
+                            key={route.title}
                             onClick={() => {
                                 goToRoute(route.path);
                             }}
                         >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
+                            <FontAwesomeIcon icon={route.icon} />
+                            <span>{route.title}</span>
                         </div>
                     ))
                 }
             </div>
-        </div>
+
+        </SidebarContainer>
     );
 };
 
